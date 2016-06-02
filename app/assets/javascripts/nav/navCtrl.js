eb4c8b3fdecd6abe7e3f4a3d977d4d3f3949ad80
Sheng Da Zhang <shengdzhang@gmail.com>
2016-06-02 02:38:41 -0700

    deviseresolved?

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -2,12 +2,12 @@ KnoWhere.controller('NavCtrl', [
   '$scope',
   'Auth',
   function($scope, Auth){
-    // $scope.signedIn = Auth.isAuthenticated;
-    // $scope.logout = Auth.logout;
-    // Auth.currentUser().then(function (user){
-    //   $scope.user = user;
-    // });
-    //
+    $scope.signedIn = Auth.isAuthenticated;
+    $scope.logout = Auth.logout;
+    Auth.currentUser().then(function (user){
+      $scope.user = user;
+    });
+
     $scope.$on('devise:new-registration', function (e, user){
       $scope.user = user;
     });
